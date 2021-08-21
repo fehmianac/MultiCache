e3b6930f35f0217c68e199beb8f20f7dd5c5f0fa
Fehmi Anaç <fehmi_anac@hotmail.com>
2021-08-21 15:57:56 +0300

    Added flag in multicache response, the result comes from cache

--- src/MultiCache/Abstractions/IMultiCacheManager.cs
+++ src/MultiCache/Abstractions/IMultiCacheManager.cs
@@ -6,9 +6,9 @@ namespace MultiCache.Abstractions
 {
     public interface IMultiCacheManager
     {
-        Task<T> GetAsync<T>(string key, CancellationToken cancellationToken = default);
+        Task<(T, bool)> GetAsync<T>(string key, CancellationToken cancellationToken = default);
         Task SetAsync<T>(string key, T value, TimeSpan? expiry = null, CancellationToken token = default);
-        Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, TimeSpan? expiry = null, CancellationToken cancellationToken = default);
+        Task<(T, bool)> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, TimeSpan? expiry = null, CancellationToken cancellationToken = default);
         Task RemoveAsync(string key, CancellationToken cancellationToken);
     }
 }
